d7a7bd8cf154f683d400acf5303b08fd34da1d7d
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2023-03-21 20:00:27 +0300

    create project

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,17 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 
-namespace VoiceKeyboard;
-
-/// <summary>
-///     Interaction logic for App.xaml
-/// </summary>
-public partial class App : Application
+namespace VoiceKeyboard
 {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
 }
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,75 +1,15 @@
-﻿using System;
-using System.Diagnostics;
-using System.Threading;
-using System.Windows;
-using VoiceKeyboard.GrpcUtils;
-using VoiceKeyboard.ViewModels;
+﻿using System.Windows;
 
-namespace VoiceKeyboard.Views;
-
-/// <summary>
-///     Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace VoiceKeyboard.Views
 {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
-        StartServer();
-
-        DataContext = new MainWindowViewModel();
-
-        Closed += OnShutdown;
-    }
-
-    private void OnShutdown(object? sender, EventArgs e)
-    {
-        ShutdownServer();
-    }
-
-    private void StartServer()
-    {
-        Trace.WriteLine("starting server");
-
-        var p = new Process();
-
-        var info =
-            new ProcessStartInfo("C:\\Users\\dm1tr\\Desktop\\VoiceKeyboard\\Server\\vk_server.exe");
-        info.Arguments = "-p windows";
-        info.RedirectStandardOutput = false;
-        info.RedirectStandardInput = false;
-        info.CreateNoWindow = true;
-        info.UseShellExecute = false;
-        p.StartInfo = info;
-
-        // p.Start();
-
-        WaitServerForStart();
-    }
-
-    private void WaitServerForStart()
-    { 
-        int errPingCount = 0;
-        while (!GrpcClientUtil.PingServer(GrpcClientUtil.ServerHost, GrpcClientUtil.ServerPort))
+        public MainWindow()
         {
-            errPingCount++;
-            if (errPingCount >= 5)
-            {
-                MessageBox.Show("Невозможно запустить локальный сервер приложения");
-                Environment.Exit(-1);
-            }
-        }
-
-        Thread.Sleep(1000);
-    }
-
-    private void ShutdownServer()
-    {
-        Trace.WriteLine("stopping server");
-        foreach (var p in Process.GetProcessesByName("vk_server"))
-        {
-            p.Kill();
-            p.WaitForExit();
+            InitializeComponent();
         }
     }
 }
